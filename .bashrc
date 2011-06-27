10b12201775dbba1992e6818803d345160af57b9
Spencer T Brody <spencer@10gen.com>
2011-06-27 11:14:47 -0400

    Initial commit of personal config files.

---
+++ .bashrc
@@ -0,0 +1,68 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# don't put duplicate lines in the history. See bash(1) for more options
+# ... or force ignoredups and ignorespace
+HISTCONTROL=ignoredups:ignorespace
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000000
+HISTFILESIZE=20000000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# Load personal environment settings
+
+if [ -f ~/.environment ]; then
+    . ~/.environment
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    . /etc/bash_completion
+fi
+
+_CORES=$(grep -c processor /proc/cpuinfo )
+_COMPILE_THREADS=$(echo $_CORES '* 3 / 2' | bc)
+
+export SCONSFLAGS="-j$_COMPILE_THREADS"
+
+#
+# Set the prompt.
+#
+PS1="\[`tput rev`\]\h\[`tput sgr0`\] \w \$ ";
+
+
+# git stuff
+source $HOME/.git-completion.bash
+export PS1='[ \w$(__git_ps1 " (%s)") ] '
+#export PS1='[ \w $(vcprompt|sed "s/\\[\\([a-zA-Z0-9]*\\):]/(\\1)/"|sed "s/\\[\\([a-z]*\\):\\([a-z]*\\)\\]/(\\1:\\2)/")] '
+
+#start screen
+if [ "$TERM" != "screen" ]; then
+     screen
+fi
---
+++ .emacs.d/init.el
@@ -0,0 +1,89 @@
+(defvar *emacs-load-start* (current-time))
+
+
+;; This gives us `third'
+(require 'cl)
+
+
+;;;;Set up emacs paths
+(setq conf-home (concat (file-name-as-directory (expand-file-name "~"))
+						(file-name-as-directory ".emacs.d")))
+
+(setq conf-tmp (concat conf-home
+                       (file-name-as-directory "tmp")))
+
+;; Add to the end of the list
+(add-to-list 'load-path (concat conf-home "elisp") t)
+
+(setq custom-file
+	  (concat conf-home "custom.el"))
+(load custom-file 'noerror)
+
+
+;;;;Set up C indentation
+(defun my-build-tab-stop-list (width)
+  (let ((num-tab-stops (/ 80 width))
+	(counter 1)
+	(ls nil))
+    (while (<= counter num-tab-stops)
+      (setq ls (cons (* width counter) ls))
+      (setq counter (1+ counter)))
+    (set (make-local-variable 'tab-stop-list) (nreverse ls))))
+(defun my-c-mode-common-hook ()
+  (setq tab-width 4) ;; change this to taste, this is what K&R uses :)
+  (my-build-tab-stop-list tab-width)
+  (setq c-basic-offset tab-width)
+  (setq indent-tabs-mode nil)) ;; force only spaces for indentation
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+
+
+(require 'linum)
+(require 'cc-mode)
+;(load-file "/usr/share/emacs/site-lisp/xcscope.el")
+(require 'xcscope)
+(setq cscope-display-cscope-buffer nil)
+
+;; Remove splash screen
+(setq inhibit-splash-screen t)
+
+(show-paren-mode t)
+(line-number-mode 1)
+(column-number-mode 1)
+(transient-mark-mode t)
+(global-linum-mode 1)
+
+(global-set-key [(f9)] 'compile)
+(global-set-key [(control tab)] 'dabbrev-expand)
+(global-set-key "\C-z" 'undo)
+
+(setq compilation-window-height 8)
+
+(setq compilation-finish-function
+      (lambda (buf str)
+
+        (if (string-match "exited abnormally" str)
+
+            ;;there were errors
+            (message "compilation errors, press C-x ` to visit")
+
+          ;;no errors, make the compilation window go away in 0.5 seconds
+          (run-at-time 0.5 nil 'delete-windows-on buf)
+          (message "NO COMPILATION ERRORS!"))))
+
+;; Time how long it took to start up.
+(let ((the-time (current-time)))
+  (message "My .emacs loaded in %dms"
+           (/ (-
+               (+
+                (third the-time)
+                (* 1000000
+                   (second the-time)))
+               (+
+                (third *emacs-load-start*)
+                (* 1000000
+                   (second *emacs-load-start*)))
+               ) 1000)))
+
+;;(message "%s" (lookup-key global-map [(meta tab)]))
+(put 'scroll-left 'disabled nil)
