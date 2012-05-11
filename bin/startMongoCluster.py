3ac2d7c9bc6803d702faf3a201ad964ef7d99d53
Spencer T Brody <spencer@10gen.com>
2012-05-11 11:13:20 -0400

    Allow startMongoCluster to pass extra flags to the processes it starts.

--- bin/startMongoCluster.py
+++ bin/startMongoCluster.py
@@ -4,8 +4,8 @@ import subprocess
 import time
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage: startMongoCluster.py <filepath>"
+    if len(sys.argv) < 2:
+        print "Usage: startMongoCluster.py <filepath> [<extraoption> ...]"
         sys.exit(-1)
 
     setupFile = sys.argv[1]
@@ -14,6 +14,8 @@ if __name__ == "__main__":
         if line == "" or line.isspace():
             continue
         (termName, termCommand) = line.rstrip('\n').split('|')
+        for option in sys.argv[2:]:
+            termCommand += " " + option
         cmd = "gnome-terminal --geometry=50x15 -t %s -x bash -c \"%s; echo -e '\\nCommand run:\\n%s'; bash;\"" % (termName, termCommand, termCommand)
         print cmd
         subprocess.Popen(cmd, shell=True)
